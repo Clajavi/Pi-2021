df96a065f72e658596e6d8d17d28868cb756ecbb
Clajavi/Pi-2021 <jhonboyddunlop@gmail.com>
2021-11-22 21:23:49 -0300

    Etapa II - delete

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import  ListView, DetailView
+from django.views.generic import  ListView,DetailView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
 from .models import Post
 class BlogListView(ListView):
     model = Post
@@ -8,6 +11,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'custom'
+    #context_object_name = 'custom'
 # Create your views here.
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields=['autor','titulo','conteudo']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields=['titulo','conteudo']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
 
